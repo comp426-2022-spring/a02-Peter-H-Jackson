c94fdad580808276683c6bae18a8158580f41bcd
Peter Jackson <peterhj@live.unc.edu>
2022-02-20 21:32:31 -0500

    created flips.js

---
+++ flips.js
@@ -0,0 +1,18 @@
+import minimist from "minimist"
+import { coinFlips } from "./modules/coin.mjs";
+
+const args = minimist(process.argv.slice(2))
+if (args.number == undefined) { args.number = 1 }
+
+let record = coinFlips(args.number)
+console.log(record)
+let heads = 0
+let tails = 0
+for (let i = 0; i < args.number; i++) {
+    if (record[i] == "heads") {
+        heads++
+    } else {
+        tails++
+    }
+}
+console.log("{ tails: " + tails +", heads:" + heads + " }")
